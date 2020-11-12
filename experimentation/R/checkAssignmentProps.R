ddadcb964e56b42162a979a7bb8ed7006ef4ca21
jhans377 <josh@loom.com>
2020-11-12 12:05:38 -0800

    updating proportion check definition

--- experimentation/R/checkAssignmentProps.R
+++ experimentation/R/checkAssignmentProps.R
@@ -10,19 +10,23 @@
 
 library("dplyr")
 
-checkAssignmentProps <- function(raw_data,segmentation,expected_split) {
+checkAssignmentProps <- function(raw_data,sample_variable,control_value,expected_split) {
 
-  segmentation = enquo(segmentation)
+  sample_variable = enquo(sample_variable)
+
+  ## take segmentation variable and set to control and variant values
+  raw_data <- raw_data %>% mutate(segmentation = ifelse(UQ(sample_variable) == control_value,'control','variant'))
 
  data <- raw_data %>%
-  group_by(UQ(segmentation)) %>%
-    summarize(obs = n_distinct(id)) %>% mutate(share = obs/sum(obs))
+  group_by(segmentation) %>%
+    summarize(obs = n_distinct(id)) %>%
+      mutate(share = obs/sum(obs))
 
  data <- data %>%
-  summarise(n_control = max(obs[sample=='control']),
-            n_variant = max(obs[sample=='variant']),
-            prop_control = max(share[sample=='control']),
-            prop_variant = max(share[sample=='variant']))
+  summarise(n_control = max(obs[segmentation=='control']),
+            n_variant = max(obs[segmentation=='variant']),
+            prop_control = max(share[segmentation=='control']),
+            prop_variant = max(share[segmentation=='variant']))
 
  return(data)
 }
