a216e1dfdfb187a743ebc1edcf30d91b4cc1dd9d
jhans377 <69058168+jhans377@users.noreply.github.com>
2020-11-30 20:14:26 -0700

    Create visualizeBayePropDailyDiff.R

---
+++ experimentation/R/visualizeBayePropDailyDiff.R
@@ -0,0 +1,21 @@
+#' This takes results from summariseMetricDaily and generates time-series view with cumulative daily totals and credible intervals.
+#'
+#'
+#' @param Yes
+#' @keywords ab test, summarise
+#' @export
+#' @examples
+#' visualizeBayesianPropDaily(bayes_daily)
+
+
+visualizeBayesianPropDaily <- function(daily_bayes) {
+
+control <- daily_bayes %>% select(date,value=rate_control,lower=lower_control,upper=upper_control) %>% mutate(sample = 'control')
+variant <- daily_bayes %>% select(date,value=rate_variant,lower=lower_variant,upper=upper_variant) %>% mutate(sample = 'variant')
+
+sample <- rbind(control,variant)
+
+view <- ggplot(daily_bayes,aes(x=date(date),y=median_diff,group=1)) + geom_line() + geom_ribbon(aes(ymin=lower_diff,ymax=upper_diff),colour='grey',alpha=0.2) + scale_y_continuous(label=scales::percent) + theme(legend.position='bottom',legend.title=element_blank()) + ylab('Estimated Delta') + xlab('Day of Experiment') + geom_hline(yintercept=0,linetype='dashed',colour='red') + ggtitle('Daily Estimate for Delta with Credible Intervals')
+
+return(view)
+}
